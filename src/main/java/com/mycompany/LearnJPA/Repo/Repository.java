e0321b06381dcb7ea47234a90b1f7c5035b53378
Ansar142 <130049687+Ansar142@users.noreply.github.com>
2025-04-18 16:05:51 +0500

    Update Repository.java

--- src/main/java/com/mycompany/LearnJPA/Repo/Repository.java
+++ src/main/java/com/mycompany/LearnJPA/Repo/Repository.java
@@ -6,4 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 @org.springframework.stereotype.Repository
 public interface Repository extends JpaRepository<Student,Integer> {
 
+  Student findByMarksGreaterThan(int marks);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Student s WHERE s.marks = :marks")
+    void deleteByMarks(int marks);
+
+
 }
